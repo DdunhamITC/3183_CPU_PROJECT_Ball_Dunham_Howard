eb1a27b5f93a4eb1993197d1f66e3630e2b6856b
DdunhamITC <49312030+DdunhamITC@users.noreply.github.com>
2019-04-12 10:30:51 -0400

    Update mem.cpp

--- mem.cpp
+++ mem.cpp
@@ -1,1 +1,65 @@
+Memory :: Memory() // constructor just needed for access
+    {}
 
+void Memory :: loading(int dat, int loc) // loading data into register
+    {
+        vector<int> vect = dec2_32BIN(dat);
+        vector<int> binNUM;
+        for (vector<int>::iterator it = vect.begin(); it !=vect.end(); ++it)
+            binNUM.push_back(*it);
+
+        for (int i = 0 ; i < binNUM.size() ; i++)
+            {
+                bNUM[i] = binNUM[i];
+            }
+        for (int i = 0 ; i < binNUM.size() ; i++)
+        {
+            m[loc][i]=bNUM[i];
+        }
+    }
+
+
+void Memory :: calling(int loc) // calling data from register
+    {
+        for (int i = 0 ; i < 32 ; i++)
+        {
+            binNUMout[i]=m[loc][i];
+        }
+    }
+
+vector <int> Memory :: dec2_32BIN(int data) //
+    {
+                vector <int> binaryNum; 
+                for (int i = 31; i >= 0; i--)
+                    {
+                        int k = data >> i; 
+                        if (k & 1) 
+                            binaryNum.push_back(1);
+                        else
+                            binaryNum.push_back(0);
+                    }
+            return binaryNum;
+    }
+
+void Memory :: resetREG()
+    {
+        for (int i = 0 ; i < size ; i++)
+            {
+                for (int j = 0 ; j < 32 ; j++)
+                    {
+                        m[i][j]=0;
+                    }
+            }
+    }
+
+void Memory :: dispREGS()
+    {
+        for (int i = 0 ; i < size ; i++)
+            {
+                for (int j = 0 ; j < 32 ; j++)
+                    {
+                        cout << m[i][j] << " ";
+                    }
+                cout << endl;
+            }
+    }
