7d38c39ab34de67fe4e0eeecaee9365ed25c7bef
DdunhamITC <49312030+DdunhamITC@users.noreply.github.com>
2019-04-22 23:38:05 -0400

    Add files via upload

---
+++ abs.cpp
@@ -0,0 +1,14 @@
+#include "abs.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> absV (vector <int> a)
+    {
+        if (a[0]==1)
+            a[0]=0;
+        else
+            a[0]=0;
+    
+        return a;
+    }
---
+++ abs.h
@@ -0,0 +1,12 @@
+#ifndef ABS_H
+#define ABS_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> absV (vector <int> a);
+
+
+
+#endif
